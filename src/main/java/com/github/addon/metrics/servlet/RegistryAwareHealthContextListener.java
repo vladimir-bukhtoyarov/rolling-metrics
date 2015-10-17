ad4dbfa1be32fa1f089e4a9ef1ec0c1913c19d39
vladimir.bukhtoyarov <jsecoder@mail.ru>
2015-10-18 00:28:21 +0300

    enhancements for health and metrics context listeners

---
+++ src/main/java/com/github/addon/metrics/servlet/RegistryAwareHealthContextListener.java
@@ -0,0 +1,25 @@
+package com.github.addon.metrics.servlet;
+
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.codahale.metrics.servlets.HealthCheckServlet;
+
+import java.util.Objects;
+
+public class RegistryAwareHealthContextListener extends HealthCheckServlet.ContextListener {
+
+    private final HealthCheckRegistry registry;
+
+    public RegistryAwareHealthContextListener(HealthCheckRegistry registry) {
+        this.registry = Objects.requireNonNull(registry);
+    }
+
+    public RegistryAwareHealthContextListener() {
+        this.registry = new HealthCheckRegistry();
+    }
+
+    @Override
+    protected HealthCheckRegistry getHealthCheckRegistry() {
+        return null;
+    }
+
+}
