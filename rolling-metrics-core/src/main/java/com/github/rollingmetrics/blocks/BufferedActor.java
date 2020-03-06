64d8891ef754caf0bc135747cb41e21106f0ee74
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2020-03-06 13:17:12 +0300

    Building block for complex metrics which can not be implemented withoud mutual exclusion. LazyActor allows to replace blocking by buffering.

---
+++ rolling-metrics-core/src/main/java/com/github/rollingmetrics/blocks/BufferedActor.java
@@ -0,0 +1,191 @@
+/*
+ *    Copyright 2020 Vladimir Bukhtoyarov
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.github.rollingmetrics.blocks;
+
+import org.jctools.queues.MpscBlockingConsumerArrayQueue;
+import org.jctools.queues.SpmcArrayQueue;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+/**
+ * Primary intention of this class is replacing the blocking by buffering for complex monitoring metrics
+ * which can not be implemented(or it is too complex to implement) as none-blocking structure.
+ * With BufferedActor you can threat your code as lock-free as long as it is enough place in the buffer,
+ * when place in the buffer is left then BufferedActor downgrades processing to the blocking paradigm,
+ * but this case should happen rarely because typical application does something useful between metric publishing,
+ * in other words it is not likely that your application spent more time in metrics logging instead of doing real work.
+ *
+ * <p>
+ * Second intention is provide garbage-free style of metrics collection.
+ * If underling data-structure does not perform memory allocation on each update,
+ * then BufferedActor will help to organize message passing in the way that does not require node allocation when placing task to the buffer.
+ *
+ * <p>
+ * Implementation details:
+ * <ul>
+ *     <li>
+ *         At initialization time BufferedActor pre-allocates buffer with actions, then actions are reused.
+ *     </li>
+ *     <li>
+ *         On each time only one thread allowed to do an action.
+ *     </li>
+ *     <li>
+ *         Instead of blocking this class does the buffering of actions if there is a thread in the progress.
+ *         That means that computation can be completed somewhere in the future,
+ *         but it is not a problem because when you update an metric you never expect the result.
+ *     </li>
+ *     <li>
+ *         Thread in progress does its work in batches instead of applying action one by one, that means that when thread finished its own action
+ *     it helps to complete actions scheduled by other threads
+ *     </li>
+ *     <li>
+ *         In order to avoid live lock(when too many parallel treads are scheduling) each active thread is allowed to process no more that {@link #batchSize} actions at once.
+ *     </li>
+ *     <li>
+ *         An thread can be blocked if and only if there are no available actions in the pool, in such case blocked tread helps to process accumulated buffer.
+ *     </li>
+ * </ul>
+ *
+ * @param <T>
+ */
+public class BufferedActor<T extends BufferedActor.ReusableActionContainer> {
+
+    private final SpmcArrayQueue<T> actionPool;
+    private final MpscBlockingConsumerArrayQueue<T> scheduledActions;
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final AtomicLong blockedCounter = new AtomicLong();
+    private final int batchSize;
+
+    /**
+     * @param actionFactory factory which used to populate action pool, pool is always populated to maximum during initialization
+     * @param bufferSize the size of queue for incoming requests
+     * @param batchSize specifies how many actions one thread is allowed to perform in single batch,
+     *                  intent of this parameter is to avoid live lock of exclusive lock owner
+     */
+    public BufferedActor(Supplier<T> actionFactory, int bufferSize, int batchSize) {
+        actionPool = new SpmcArrayQueue<>(bufferSize);
+        scheduledActions = new MpscBlockingConsumerArrayQueue<>(bufferSize);
+        this.batchSize = batchSize;
+
+        for (int i = 0; i < bufferSize; i++) {
+            T action = actionFactory.get();
+            actionPool.add(action);
+        }
+    }
+
+    public final T getActionFromPool() {
+        T action = actionPool.poll();
+        if (action != null) {
+            // fast path
+            return action;
+        }
+
+        // if there are no actions in pool we need to help to process already scheduled actions
+        blockedCounter.incrementAndGet();
+        lock.lock();
+
+        boolean actionFromPool = false;
+        T firstAction = null;
+        while (firstAction == null) {
+            firstAction = actionPool.poll();
+            if (firstAction != null) {
+                actionFromPool = true;
+                break;
+            }
+            try {
+                firstAction = scheduledActions.take();
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
+
+        int dispatchedActions = 0;
+        T currentAction = firstAction;
+        while (currentAction != null) {
+            if (currentAction != firstAction || !actionFromPool) {
+                currentAction.run();
+            }
+            if (currentAction != firstAction) {
+                currentAction.prepareForReuse();
+                actionPool.offer(currentAction);
+            }
+            if (dispatchedActions++ >= batchSize) {
+                break;
+            }
+            currentAction = scheduledActions.poll();
+        }
+        lock.unlock();
+
+        firstAction.prepareForReuse();
+        return firstAction;
+    }
+
+    public void doExclusivelyOrSchedule(T action) {
+        scheduledActions.offer(action);
+
+        if (!lock.tryLock()) {
+            return;
+        }
+
+        int dispatchedActions = 0;
+
+        T firstAction = scheduledActions.poll();
+        T currentAction = firstAction;
+
+        while (currentAction != null) {
+            currentAction.run();
+            if (currentAction != firstAction) {
+                currentAction.prepareForReuse();
+                actionPool.offer(currentAction);
+            }
+            if (dispatchedActions++ >= batchSize) {
+                break;
+            }
+            currentAction = scheduledActions.poll();
+        }
+
+        if (firstAction != null) {
+            firstAction.prepareForReuse();
+            actionPool.offer(firstAction);
+        }
+        lock.unlock();
+    }
+
+    public long getBlockedCount() {
+        return blockedCounter.get();
+    }
+
+    public int getActionPoolSize() {
+        return actionPool.size();
+    }
+
+    public int getScheduledActionsCount() {
+        return scheduledActions.size();
+    }
+
+    public interface ReusableActionContainer {
+
+        void prepareForReuse();
+
+        void run();
+
+    }
+
+}
---
+++ rolling-metrics-core/src/test/java/com/github/rollingmetrics/blocks/BufferedActorTest.java
@@ -0,0 +1,101 @@
+/*
+ *    Copyright 2020 Vladimir Bukhtoyarov
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.github.rollingmetrics.blocks;
+
+import org.junit.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static org.junit.Assert.*;
+
+public class BufferedActorTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        int sharedSum[] = new int[1];
+
+        BufferedActor[] actorRef = new BufferedActor[1];
+
+        Supplier<BufferedActor.ReusableActionContainer> actionFactory = () -> new BufferedActor.ReusableActionContainer() {
+            @Override
+            public void prepareForReuse() {
+            }
+
+            @Override
+            public void run() {
+                sharedSum[0] = sharedSum[0] + 1;
+
+//                if (sharedSum[0]%1_000 == 0) {
+//                    System.out.println(sharedSum[0] + ":" + Thread.currentThread().getName() + " action pool size=" + actorRef[0].getActionPoolSize() + " in progress action count " + actorRef[0].getScheduledActionsCount() + " overflow count " + actorRef[0].getBlockedCount());
+//                }
+            }
+        };
+
+        BufferedActor<BufferedActor.ReusableActionContainer> actor = new BufferedActor<>(actionFactory, 512, 1024);
+        actorRef[0] = actor;
+
+        int iterationsPerThread = 10_000_000;
+        Thread[] threads = new Thread[4];
+        CountDownLatch latch = new CountDownLatch(threads.length);
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                latch.countDown();
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                for (int j = 0; j < iterationsPerThread; j++) {
+                    BufferedActor.ReusableActionContainer action = actor.getActionFromPool();
+                    actor.doExclusivelyOrSchedule(action);
+                    if (j % 1000 == 0) {
+                        try {
+                            TimeUnit.NANOSECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            threads[i].setName("Incrementor-" + (i+1));
+            threads[i].start();
+        }
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(sharedSum[0] + ":" + " action pool size=" + actorRef[0].getActionPoolSize() + " in progress action count " + actorRef[0].getScheduledActionsCount() + " blocked count " + actorRef[0].getBlockedCount());
+            }
+        }, 1, 1000);
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+        timer.cancel();
+
+        System.out.println("result sum:" + sharedSum[0]);
+        System.out.println("blocked count:" + actor.getBlockedCount());
+
+        assertEquals(sharedSum[0], iterationsPerThread * threads.length);
+    }
+
+}
